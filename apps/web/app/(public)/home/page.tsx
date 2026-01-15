7e42014f3e4983007558569e46653f2c65229e80
Snehal Pradhan <snehalpradhan23@gmail.com>
2026-01-16 02:20:07 +0530

    feat: add Homepage component and configure Tailwind CSS

---
+++ apps/web/app/(public)/home/page.tsx
@@ -0,0 +1,9 @@
+function Homepage() {
+  return (
+    <div className="flex flex-col justify-center items-center text-gray-900 min-h-screen bg-gray-100">
+      getGuide starting will be ready soon...
+    </div>
+  );
+}
+
+export default Homepage;
